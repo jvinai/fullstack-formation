f92f4866aa0115db5b15a98d14b5818a88a4c1d0
Jeremy Vinai <jvinai@xebia.fr>
2015-08-21 12:03:21 +0200

    Init exo-node-3 branch

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var ideas = [{
+  text: 'I should buy a boat',
+  priority: 1,
+  isDone: false,
+  date: new Date()
+}, {
+  text: 'I should write a book',
+  priority: 2,
+  isDone: false,
+  date: new Date()
+}];
+
+app.get('/api/getAll', function (req, res) {
+  res.send(ideas);
 });
 
 app.listen(3000);
