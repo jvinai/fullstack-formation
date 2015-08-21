24542ca1965a5d2d816fa356f2fd5c3940726e5b
Jeremy Vinai <jvinai@xebia.fr>
2015-08-21 12:09:37 +0200

    init exo-node-4 branch

--- server.js
+++ server.js
@@ -3,18 +3,26 @@ var app = express();
 
 var ideas = [{
   text: 'I should buy a boat',
-  priority: 1,
+  priority: 2,
   isDone: false,
   date: new Date()
 }, {
   text: 'I should write a book',
-  priority: 2,
+  priority: 3,
+  isDone: false,
+  date: new Date()
+}, {
+  text: 'I should refactor my code',
+  priority: 1,
   isDone: false,
   date: new Date()
 }];
 
 app.get('/api/getAll', function (req, res) {
-  res.send(ideas);
+  var sortedIdeas = ideas.sort(function (a, b) {
+    return a.priority > b.priority;
+  });
+  res.send(sortedIdeas);
 });
 
 app.listen(3000);
