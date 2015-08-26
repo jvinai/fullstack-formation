8377869ac6109fe5837111f137cfbb9bcffda761
Jeremy Vinai <jvinai@xebia.fr>
2015-08-26 15:01:17 +0200

    init exo-angular-3

--- front/controller/myController.js
+++ front/controller/myController.js
@@ -1,5 +1,21 @@
 'use strict';
 
-angular.module("myIdeaBox").controller('myCtrl', function () {
-  console.log("hello from controller!");
+angular.module("myIdeaBox").controller('myCtrl', function ($scope) {
+  $scope.ideas = [{
+    text: 'I should buy a boat',
+    priority: 2,
+    isDone: false,
+    date: new Date()
+  }, {
+    text: 'I should write a book',
+    priority: 3,
+    isDone: false,
+    date: new Date()
+  }, {
+    text: 'I should refactor my code',
+    priority: 1,
+    isDone: false,
+    date: new Date()
+  }];
+
 });
