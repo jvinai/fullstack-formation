9080ff93e233de3948de0f011b3a5eae9d668da3
Jeremy Vinai <jvinai@xebia.fr>
2015-08-27 15:18:49 +0200

    init exo-angular-6

--- front/app.js
+++ front/app.js
@@ -1,1 +1,11 @@
-angular.module("myIdeaBox", ['ui.bootstrap']);
+angular.module("myIdeaBox", ['ui.bootstrap', 'ngRoute']).config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'front/template/ideas.html',
+      controller: 'myCtrl as c'
+    })
+    .when('/add', {
+      templateUrl: 'front/template/add.html'
+    })
+    .otherwise({templateUrl: 'default.html'})
+});
